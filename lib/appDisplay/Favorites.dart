64af6a432140d4b83b419d2101a1ebffab3da7ae
leilaale <leilaalemtz.11@gmail.com>
2023-08-04 13:44:19 -0600

    Added Drawer, Favorites Page, Built Cars Page

---
+++ lib/appDisplay/Favorites.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Favorites extends StatefulWidget {
+
+  @override
+  State<Favorites> createState() => _Favorites();
+}
+
+class _Favorites extends State<Favorites> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("FAVORITES"),
+    );
+  }
+
+}
